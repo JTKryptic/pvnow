7308d9cd6ef2e3e033c0e6ba465c781d292dd5f9
Javon Thomas <javont123@gmail.com>
2024-01-23 17:47:41 -0600

    Added PV Color Variables

---
+++ lib/theme/pv_colors.dart
@@ -0,0 +1,5 @@
+import "package:flutter/material.dart";
+
+Color pvPurpleLight = const Color.fromRGBO(212, 167, 253, 1);
+Color pvPurpleDark = const Color.fromRGBO(96, 38, 150, 1);
+Color pvGold = const Color.fromRGBO(255, 234, 8, 1);
