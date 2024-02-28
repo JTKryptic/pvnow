dae90b146b3ae23e1d15d628ca4749070e21654f
javont123 <javont123@gmail.com>
2024-02-28 00:11:57 +0000

    Android emulator fix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,46 +1,22 @@
-{pkgs}: {
+{pkgs, ...}: {
   channel = "stable-23.11";
   packages = [
     pkgs.flutter
     pkgs.jdk17
   ];
-  idx.extensions = [
-    
-  ];
+  idx.extensions = [];
   idx.previews = {
+    enable = true;
     previews = [
-      {
-        command = [
-          "flutter"
-          "run"
-          "--machine"
-          "-d"
-          "web-server"
-          "--web-hostname"
-          "0.0.0.0"
-          "--web-port"
-          "$PORT"
-        ];
-        id = "web";
-        manager = "flutter";
-      }
-      {
-        command = [
-          "flutter"
-          "run"
-          "--machine"
-          "-d"
-          "android"
-          "-d"
-          "emulator-5554"
-        ];
-        id = "android";
-        manager = "flutter";
-      }
-      {
-        id = "ios";
-        manager = "ios";
-      }
+        {
+            command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+            id = "android";
+            manager = "flutter";
+        }
+        {
+            id = "ios";
+            manager = "ios";
+        }
     ];
   };
 }
