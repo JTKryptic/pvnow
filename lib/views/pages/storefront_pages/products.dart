2390fdac4d7fa4ec67d4a82757a885249ae695bf
SanaiRF <104601561+SanaiRF@users.noreply.github.com>
2024-04-20 19:40:30 -0500

    redone products page

--- lib/views/pages/storefront_pages/products.dart
+++ lib/views/pages/storefront_pages/products.dart
@@ -27,6 +27,227 @@ class _StorefrontPageState extends State<StorefrontPage> {
         ],
       ),
       drawer: MyDrawer(),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //product image
+                AspectRatio(
+                  aspectRatio: 1.2,
+                  child: Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(5),
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset('lib/images/8.png')
+                      //Icon(Icons.propane)
+                      ),
+                ),
+                SizedBox(height: 5),
+                //product name
+                Text(
+                  "Product",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                //desc
+                //Text("Description of product"),
+                //price
+                //Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //product image
+                AspectRatio(
+                  aspectRatio: 1.2,
+                  child: Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(5),
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset('lib/images/5.png')
+                      //Icon(Icons.propane)
+                      ),
+                ),
+                SizedBox(height: 5),
+                //product name
+                Text(
+                  "Product",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                //desc
+                //Text("Description of product"),
+                //price
+                //Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //product image
+                AspectRatio(
+                  aspectRatio: 1.2,
+                  child: Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(5),
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset('lib/images/1.png')
+                      //Icon(Icons.propane)
+                      ),
+                ),
+                SizedBox(height: 5),
+                //product name
+                Text(
+                  "Product",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                //desc
+                //Text("Description of product"),
+                //price
+                //Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //product image
+                AspectRatio(
+                  aspectRatio: 1.2,
+                  child: Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(5),
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset('lib/images/1.png')
+                      //Icon(Icons.propane)
+                      ),
+                ),
+                SizedBox(height: 5),
+                //product name
+                Text(
+                  "Product",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                //desc
+                //Text("Description of product"),
+                //price
+                //Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //product image
+                AspectRatio(
+                  aspectRatio: 1.2,
+                  child: Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(5),
+                      margin: const EdgeInsets.all(5),
+                      child: Image.asset('lib/images/1.png')
+                      //Icon(Icons.propane)
+                      ),
+                ),
+                SizedBox(height: 5),
+                //product name
+                Text(
+                  "Product",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                //desc
+                //Text("Description of product"),
+                //price
+                //Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              children: [
+                //product image
+                Icon(Icons.propane),
+                //product name
+                Text("Product", style: TextStyle(fontWeight: FontWeight.bold)),
+                //desc
+                Text("Description of product"),
+                //price
+                Text("\$20"),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              children: [
+                //product image
+
+                Icon(Icons.propane),
+                //product name
+                Text("Product", style: TextStyle(fontWeight: FontWeight.bold)),
+                //desc
+                Text("Description of product"),
+                //price
+                Text("20"),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
